2757774c82bf1f65d5d367438c5465029be46e1a
Marwan Aziz <aziz.marwan@gmail.com>
2019-04-15 16:55:56 -0400

    new commit

--- src/equifax.ts
+++ src/equifax.ts
@@ -3,23 +3,7 @@ import {Page} from "puppeteer";
 
 const {Equifax_user, Equifax_password} = process.env;
 
-const navigate = async (page: Page, url: string) => {
-    await page.setViewport({width: 1375, height: 1000});
-    await page.goto(url, {waitUntil: "networkidle2"});
-    const username = await page.$('#userName');
-    await username.type(Equifax_user);
-    const password = await page.$('#consumerPwd');
-    await password.type(Equifax_password);
-    console.log('entered credentials');
-
-
-    await Promise.all([
-        page.waitForNavigation({waitUntil: "networkidle2"}),
-        page.keyboard.press('Enter')
-    ]);
-
-
-    console.log('loaded main page');
+const getScore = async (page: Page)  => {
     await page.waitForSelector('#ppeData > div.agencyBoxScaleMid > p.bigScore');
     const score = await page.evaluate(() =>
         document.querySelector('#ppeData > div.agencyBoxScaleMid > p.bigScore').textContent
@@ -27,10 +11,30 @@ const navigate = async (page: Page, url: string) => {
     const status = await page.evaluate(() =>
         document.querySelector('#ppeData > div.agencyBoxScaleMid > p.bigScoreDesc').textContent
     );
-    console.log(score, status);
+    return {
+        score,
+        status
+    }
+};
+const login = async (page: Page, url: string, username: string, password: string) => {
+    await page.setViewport({width: 1375, height: 1000});
+    await page.goto(url, {waitUntil: "networkidle2"});
+    const usernameField = await page.$('#userName');
+    await usernameField.type(username);
+    const passwordField = await page.$('#consumerPwd');
+    await passwordField.type(password);
+    console.log('entered credentials');
+    await Promise.all([
+        page.waitForNavigation({waitUntil: "networkidle2"}),
+        page.keyboard.press('Enter')
+    ]);
+    console.log('loaded main page');
 };
 
 (async () => {
     const page = await init();
-    await navigate(page, 'https://www.econsumer.equifax.ca/canadaotc/showmyequifax.ehtml?locale_code=en_ca&_ga=2.19025976.1079285246.1555198618-331063922.1555198618');
+    const loginUrl = 'https://www.econsumer.equifax.ca/canadaotc/showmyequifax.ehtml?locale_code=en_ca&_ga=2.19025976.1079285246.1555198618-331063922.1555198618';
+    await login(page, loginUrl, Equifax_user, Equifax_password);
+    const score = await getScore(page);
+    console.log(score);
 })();
